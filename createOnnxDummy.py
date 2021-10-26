211e2adff575c75ab276ee483e4a933776b2449a
Kenji Asaba <kasa@akiya-souken.co.jp>
2021-10-26 17:31:00 +0900

    update dummy

--- createOnnxDummy.py
+++ createOnnxDummy.py
@@ -1,38 +1,32 @@
-import numpy as np
 import torch
-import cv2
-import sys
 
-class DummyModel(torch.nn.Module):
+# Model definition
+class PreprocessModel(torch.nn.Module):
     def forward(self, x):
-        x = x[:,:,:,[2, 1, 0]] # BRG to RGB
-        x = x.permute(0,3,1,2).float() / 255. # NHWC to NCHW, Byte to float, [0, 255] to [0, 1]
-        x2 = x * 2;
-        x3 = x * 3;
-        return x2, x3 
+        # BRG to RGB
+        x = x[:,:,:,[2, 1, 0]]
+        # NHWC to NCHW, Byte to float, [0, 255] to [0, 1]
+        x = x.permute(0,3,1,2).float() / 255.
+        # Output [1,256,256,3] RGB float
+        return x
 
-gpu = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-torch.set_grad_enabled(False)
-
-model = DummyModel().to(gpu)
-
-##############################################################################
+# Input shape definition
 batch_size = 1
 height = 256
 width = 256
-x = torch.randn((batch_size, height, width, 3), requires_grad=True).byte().to(gpu)
-##############################################################################
+channel = 3
 
-input_names = ["inputName"] #[B,192,192,3],
-output_names = ['xTimes2', 'xTimes3'] #[B,486,3], [B]
+# I/O names definition
+input_names = ["inputByteArray"]
+output_names = ['outputFloatArray']
 
-onnx_file_name = "Dummy_{}_{}_{}_xByte.onnx".format(batch_size, height, width)
-dynamic_axes = {
-    "inputName": {0: "batch_size"}, 
-    "xTimes2": {0: "batch_size"}, 
-    "xTimes3": {0: "batch_size"}
-    }
+# ONNX file name definition
+onnx_file_name = "Preprocess{}x{}x{}xBGRxByte.onnx".format(batch_size, height, width)
 
+# Export ONNX model
+cpu = torch.device("cpu")
+model = PreprocessModel().to(cpu)
+x = torch.randn((batch_size, height, width, channel)).byte().to(cpu)
 torch.onnx.export(model,
                 x,
                 onnx_file_name,
@@ -41,6 +35,5 @@ torch.onnx.export(model,
                 do_constant_folding=True,
                 input_names=input_names, 
                 output_names=output_names
-                #,dynamic_axes=dynamic_axes
                 )
 print('Onnx model exporting done')
