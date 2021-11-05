70277c21be866cd7cc9361d42d5ec48809aca97f
Kenji Asaba <kasa@akiya-souken.co.jp>
2021-11-05 17:28:19 +0900

    pose detection opset12

--- createOnnxPoseDetection.py
+++ createOnnxPoseDetection.py
@@ -20,12 +20,13 @@ batch_size = 1
 height = 128
 width = 128
 x = torch.randn((batch_size, height, width, 3), requires_grad=True).byte().to(gpu)
+opset = 12
 ##############################################################################
 
 input_names = ["input"] #[B,192,192,3],
 output_names = ['detection', 'confidence'] #[B,486,3], [B]
 
-onnx_file_name = "BlazePoseDetection_1x128x128xBGRxByte.onnx".format(batch_size, height, width)
+onnx_file_name = "BlazePoseDetection_{}x{}x{}xBGRxByte_opset{}.onnx".format(batch_size, height, width, opset)
 dynamic_axes = {
     "input": {0: "batch_size"}, 
     "detection": {0: "batch_size"}, 
@@ -36,7 +37,7 @@ torch.onnx.export(model,
                 x,
                 onnx_file_name,
                 export_params=True,
-                opset_version=11,
+                opset_version=opset,
                 do_constant_folding=True,
                 input_names=input_names, 
                 output_names=output_names
